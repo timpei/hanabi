167c32e8da8eef1707e15e2d0eef37b29ac16f32
Tim Pei <timtimpei@hotmail.com>
2014-10-25 03:40:53 -0700

    moved db class to utils

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-# all the imports
 import os, sys
-import psycopg2
-import urlparse
 import json
 import logging
 
-from threading import Thread
-from flask import Flask, g, render_template, jsonify, request
-from flask.ext.socketio import SocketIO, emit, send, join_room, leave_room
+from flask import Flask, render_template, jsonify, request
+from flask.ext.socketio import SocketIO, send, join_room, leave_room
 
 import hanabi
-from app_utils import encapsulate, parsePlayer
+from app_utils import parsePlayer, DatabaseService, getGame
 
 DEBUG = True
 
@@ -18,72 +14,6 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 socketio = SocketIO(app)
 logging.basicConfig()
-
-class DatabaseService:
-    def connect_db(self):
-        urlparse.uses_netloc.append("postgres")
-        url = urlparse.urlparse(os.environ["DATABASE_URL"])
-        conn = psycopg2.connect(
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-        )
-        return conn
-
-    def __init__(self):
-        self.conn = connect_db()
-        self.cur = self.conn.cursor()
-
-    def fetchone(self, query):
-        self.cur.execute(query)
-        return self.cur.fetchone()
-
-    def fetchall(self, query):
-        self.cur.execute(query)
-        return self.cur.fetchall()
-
-    def execute(self, query):
-        self.cur.execute(query)
-        self.conn.commit()
-
-    def bulkExecute(self, queries):
-        for query in queries:
-            self.cur.execute(query)
-        self.conn.commit()
-
-    def executeWithId(self, query):
-        self.cur.execute(query)
-        ret = self.cur.fetchone()[0]
-        self.conn.commit()
-        return ret
-
-    def close(self):
-        self.conn.close()
-        self.cur.close()
-
-
-def connect_db():
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port
-    )
-    return conn
-
-def getGame(gameId):
-    db = DatabaseService()
-    game = json.loads(db.fetchone('SELECT gameJSON FROM games WHERE id = %d; ' % gameId)[0])
-    players = db.fetchall('SELECT name, handJSON FROM players WHERE gameId = %d AND joined=1; ' % gameId)
-    spectators = db.fetchall('SELECT name FROM players WHERE gameId = %d AND joined=0; ' % gameId)
-    db.close()
-
-    return encapsulate(game, gameId, players, spectators)
 
 @app.route('/api/game/<int:gameId>', methods=['GET'])
 def loadGame(gameId):
--- app_utils.py
+++ app_utils.py
@@ -1,4 +1,7 @@
 import json
+import urlparse
+import psycopg2
+import os
 
 def parsePlayer(playerRes):
     return {
@@ -11,13 +14,63 @@ def parseSpectator(spectatorsRes):
         'name': spectatorsRes[0]
     }
 
-def encapsulate(game, gameId, playersRes, spectatorsRes):
+class DatabaseService:
+    def connect_db(self):
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(os.environ["DATABASE_URL"])
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+        return conn
+
+    def __init__(self):
+        self.conn = self.connect_db()
+        self.cur = self.conn.cursor()
+
+    def fetchone(self, query):
+        self.cur.execute(query)
+        return self.cur.fetchone()
+
+    def fetchall(self, query):
+        self.cur.execute(query)
+        return self.cur.fetchall()
+
+    def execute(self, query):
+        self.cur.execute(query)
+        self.conn.commit()
+
+    def bulkExecute(self, queries):
+        for query in queries:
+            self.cur.execute(query)
+        self.conn.commit()
+
+    def executeWithId(self, query):
+        self.cur.execute(query)
+        ret = self.cur.fetchone()[0]
+        self.conn.commit()
+        return ret
+
+    def close(self):
+        self.conn.close()
+        self.cur.close()
+
+def getGame(gameId):
+    db = DatabaseService()
+    game = json.loads(db.fetchone('SELECT gameJSON FROM games WHERE id = %d; ' % gameId)[0])
+    players = db.fetchall('SELECT name, handJSON FROM players WHERE gameId = %d AND joined=1; ' % gameId)
+    spectators = db.fetchall('SELECT name FROM players WHERE gameId = %d AND joined=0; ' % gameId)
+    db.close()
+
     game['id'] = gameId
     game['players'] = []
     game['spectators'] = []
-    for player in playersRes:
+    for player in players:
         game['players'].append(parsePlayer(player))
-    for spectator in spectatorsRes:
+    for spectator in spectators:
         game['spectators'].append(parseSpectator(spectator))
 
     return game
