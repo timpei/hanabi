2a2c9a60035d6276f9411dd9d83457b607733b63
Tim Pei <timtimpei@hotmail.com>
2014-11-05 23:31:25 -0800

    [CLIENT] minor fix to player hints display

--- app/static/js/controllers/gameTableController.js
+++ app/static/js/controllers/gameTableController.js
@@ -2,7 +2,7 @@ hanabiApp.controller('gameTableController', ['$scope', 'socketio', function($sco
   $scope.option = ''
   $scope.playerPos = getPlayerIndex()
   $scope.joinedGame = false
-  $scope.viewThirdPerspective = []
+  $scope.viewThirdPerspective = {}
 
   $scope.playForm = {
     cardIndex: -1
